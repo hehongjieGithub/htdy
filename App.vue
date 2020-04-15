75555d846b0af463becc682f9c71d5c852ce9dc4
HHJ <627759366@qq.com>
2020-04-15 14:47:03 +0800

    test

---
+++ App.vue
@@ -0,0 +1,31 @@
+<script>
+	export default {
+		globalData: {
+		            phone:"",//用户账号
+		        },
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+			const { windowWidth, windowHeight } = uni.getSystemInfoSync();
+			console.log(windowWidth, windowHeight)
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		onPageScroll(e){
+		        console.log(e)  
+		    },
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
